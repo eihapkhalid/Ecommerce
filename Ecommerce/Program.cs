5ada9ae84939e8a339b8a19d839cfe9a935b947e
eihapKhalid <eihapinfo@gmail.com>
2023-07-06 11:43:14 +0300

    Setup ApplicationDbContext

--- Ecommerce/Program.cs
+++ Ecommerce/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Ecommerce.DataAccess.Data;
 namespace Ecommerce
 {
     public class Program
@@ -8,6 +10,8 @@ namespace Ecommerce
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
