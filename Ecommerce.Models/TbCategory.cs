99813314ffc9429b9954a1e264429298403a14f5
eihapKhalid <eihapinfo@gmail.com>
2023-07-06 11:00:31 +0300

    Create TbCategory model with Data Annotations

---
+++ Ecommerce.Models/TbCategory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Models
+{
+    public class TbCategory
+    {
+        
+
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Category Name is Required")]
+        [StringLength(50, ErrorMessage = "Length must be less than 50")]
+        public string CategoryName { get; set; }
+
+        [Required]
+        public int CategoryCurrentState { get; set; }
+
+
+        public int DepartmentId { get; set; }
+
+    }
+}
