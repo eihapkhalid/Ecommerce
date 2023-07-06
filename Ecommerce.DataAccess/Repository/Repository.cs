737c513293bafcdcbcd1b746a3efc174083c75cc
Eihap Khalid <Eihapinfo@gmail.com>
2023-07-06 18:36:13 +0300

    Implement Repository

---
+++ Ecommerce.DataAccess/Repository/Repository.cs
@@ -0,0 +1,97 @@
+﻿using Ecommerce.DataAccess.Data;
+using Ecommerce.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        #region dependency injection
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+            //_db.Categories == dbSet
+            // _db.Products.Include(u => u.Category).Include(u => u.CategoryId);
+
+        } 
+        #endregion
+
+        #region Add
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+        #endregion
+
+        #region Get
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
+        {
+            IQueryable<T> query;
+            if (tracked)
+            {
+                query = dbSet;
+
+            }
+            else
+            {
+                query = dbSet.AsNoTracking();
+            }
+
+            query = query.Where(filter);
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+            return query.FirstOrDefault();
+
+        }
+        #endregion
+
+        #region GetAll
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter, string? includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+            return query.ToList();
+        }
+        #endregion
+
+        #region Remove
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+        #endregion
+
+        #region RemoveRange
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        } 
+        #endregion
+    }
+}
