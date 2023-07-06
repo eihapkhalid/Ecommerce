d108408764fc1b20344127beaa4b7947c7354eb8
Eihap Khalid <Eihapinfo@gmail.com>
2023-07-06 18:40:07 +0300

    Implement TbDepartment Repository

---
+++ Ecommerce.DataAccess/Repository/DepartmentRepository.cs
@@ -0,0 +1,29 @@
+﻿using Ecommerce.DataAccess.Data;
+using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DataAccess.Repository
+{
+    public class DepartmentRepository : Repository<TbDepartment>, IDepartmentRepository
+    {
+        #region dependency injection
+        private ApplicationDbContext _db;
+        public DepartmentRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        } 
+        #endregion
+
+        #region Update
+        public void Update(TbDepartment obj)
+        {
+            _db.TbDepartments.Update(obj);
+        } 
+        #endregion
+    }
+}
