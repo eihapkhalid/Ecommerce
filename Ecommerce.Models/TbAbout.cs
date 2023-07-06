6f1485ca8f1fc550fc4715b7ef318de81ecc00ca
eihapKhalid <eihapinfo@gmail.com>
2023-07-06 10:55:00 +0300

    Create TbTeam Model with Data Annotations. Creat relations : one TbAbout has list of TbTeam

--- Ecommerce.Models/TbAbout.cs
+++ Ecommerce.Models/TbAbout.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Ecommerce.Models
 {
-    internal class TbAbout
+    public class TbAbout
     {
+        public TbAbout()
+        {
+            ICollection<TbTeam> _TbTeam = new HashSet<TbTeam>();
+        }
+        [Key]
+        public int AboutId { get; set; }
+
+        [StringLength(100, ErrorMessage = "imge must be less than 100")]
+        public string AboutUsImg { get; set; }
+
+        [StringLength(100, ErrorMessage = "imge must be less than 100")]
+        public string AboutDescriptionImg { get; set; }
+
+        [Required(ErrorMessage = "Titel Description is Required")]
+        [StringLength(100, ErrorMessage = "Titel Description be less than 100")]
+        public string AboutTitelDescription { get; set; }
+
+        [Required(ErrorMessage = "Description is Required")]
+        [StringLength(300, ErrorMessage = "Description must be less than 300")]
+        public string AboutDescription { get; set; }
+
+        [Required(ErrorMessage = "Happy Customer Number is Required")]
+        public int AboutHappyCustomer { get; set; }
+
+
+        [Required(ErrorMessage = "Hours WorkedNumber is Required")]
+        public int AboutHoursWorked { get; set; }
+
+
+        [Required(ErrorMessage = "Awards Winned Number is Required")]
+        public int AboutAwardsWinned { get; set; }
+
+
+        [Required(ErrorMessage = "Project Done Number is Required")]
+        public int AboutProjectDone { get; set; }
+
+        [Required]
+        public int AboutCurrentState { get; set; }
+        /*******************************************/
+        public virtual ICollection<TbTeam> _TbTeam { get; set; }
     }
 }
---
+++ Ecommerce.Models/TbTeam.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Models
+{
+    public class TbTeam
+    {
+        public TbTeam()
+        {
+            TbAbout _TbAbout = new TbAbout();
+        }
+
+        [Key]
+        public int TeamId { get; set; }
+
+        [StringLength(100, ErrorMessage = "Team Name must be less than 100")]
+        public string TeamName { get; set; }
+
+        [StringLength(100, ErrorMessage = "Team Title must be less than 100")]
+        public string TeamTitle { get; set; }
+
+        [StringLength(100, ErrorMessage = "Team Description must be less than 100")]
+        public string TeamDescription { get; set; }
+
+        [StringLength(100, ErrorMessage = "imge must be less than 100")]
+        public string TeamImg { get; set; }
+
+        /*****************************************/
+        public int AboutId { get; set; }
+        public virtual TbAbout _TbAbout { get; set; }
+    }
+}
