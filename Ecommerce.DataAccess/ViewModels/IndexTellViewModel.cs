3e78da883fb40d509c2399fca2b487262a6bc4f5
Eihap Khalid <Eihapinfo@gmail.com>
2023-07-07 09:16:59 +0300

    update view model folder: - About -Tell

---
+++ Ecommerce.DataAccess/ViewModels/IndexTellViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DataAccess.ViewModels
+{
+    public class IndexTellViewModel
+    {
+        public LayoutViewModel Layout { get; set; }
+        public List<TbCompanyInformation> lstTbCompanyInformations { get; set; }
+        public TbTellUs InpTbTellUs { get; set; }
+    }
+}
