07b73c4843ef81e8a2a8d04a0f1d42ef6228eb8d
eihapKhalid <eihapinfo@gmail.com>
2023-07-12 16:50:30 +0300

    create FooterNavigationViewComponent

---
+++ Ecommerce/Areas/Customer/Components/FooterNavigationViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.DataAccess.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Areas.Customer.ViewComponents
+{
+    public class FooterNavigationViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FooterNavigationViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var viewModel = new LayoutViewModel
+            {
+                lstTbCategorys = _unitOfWork.TbCategory.GetAll().ToList(),
+                lstTbTools = _unitOfWork.TbTool.GetAll().ToList(),
+                lstTbCompanyInformations = _unitOfWork.TbCompanyInformation.GetAll().ToList(),
+                lstTbLanguages = _unitOfWork.TbLanguage.GetAll().ToList(),
+                lstTbCurrencys = _unitOfWork.TbCurrency.GetAll().ToList(),
+                lstTbDepartments = _unitOfWork.TbDepartment.GetAll().ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
