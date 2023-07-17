0896dfae91ba966d6dbb3083e5324b537433d5a2
eihapKhalid <eihapinfo@gmail.com>
2023-07-17 15:52:12 +0300

    create Tool View Model file

---
+++ Ecommerce.Models/ViewModels/ToolViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Ecommerce.Models.ViewModels
+{
+    public class ToolViewModel
+    {
+        public IEnumerable<SelectListItem> LisTbCategory { get; set; }
+        public List<TbTool> LisTbTool { get; set; }
+
+        public TbTool inpTbTool { get; set; }
+    }
+}
