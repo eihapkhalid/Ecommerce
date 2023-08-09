391df68d2d516565e188358452e33050168b4ce8
eihapKhalid <eihapinfo@gmail.com>
2023-08-09 16:31:03 +0300

    create New arrivw product table and repo pattern

---
+++ Ecommerce.DataAccess/Repository/NewArrivalProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using Ecommerce.DataAccess.Data;
+using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DataAccess.Repository
+{
+    public class NewArrivalProductRepository : Repository<TbNewArrivalProduct>, INewArrivalProductRepository
+    {
+        #region dependency injection
+        private ApplicationDbContext _db;
+        public NewArrivalProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        #endregion
+
+        #region Update
+        public void Update(TbNewArrivalProduct obj)
+        {
+            _db.TbNewArrivalProducts.Update(obj);
+        }
+        #endregion
+    }
+}
---
+++ Ecommerce.Models/TbNewArrivalProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Models
+{
+    public class TbNewArrivalProduct
+    {
+        public TbNewArrivalProduct()
+        {
+            ICollection<TbTool> _TbTool = new HashSet<TbTool>();
+        }
+
+        [Key]
+        public int NewArrivalProductId { get; set; }
+
+        [Required]
+        public int NewArrivalProductCurrentState { get; set; }
+
+        /*************************************/
+        public virtual ICollection<TbTool> _TbTool { get; set; }
+    }
+}
