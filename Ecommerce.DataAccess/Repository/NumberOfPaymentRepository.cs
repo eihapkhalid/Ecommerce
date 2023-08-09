21d995bc3af6569d416c31a1f510bcf5ac904416
eihapKhalid <eihapinfo@gmail.com>
2023-08-09 16:31:35 +0300

    create number of payment table and repo pattern

---
+++ Ecommerce.DataAccess/Repository/NumberOfPaymentRepository.cs
@@ -0,0 +1,29 @@
+﻿using Ecommerce.DataAccess.Data;
+using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DataAccess.Repository
+{
+    public class NumberOfPaymentRepository : Repository<TbNumberOfPayment>, INumberOfPaymentRepository
+    {
+        #region dependency injection
+        private ApplicationDbContext _db;
+        public NumberOfPaymentRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        #endregion
+
+        #region Update
+        public void Update(TbNumberOfPayment obj)
+        {
+            _db.TbNumberOfPayments.Update(obj);
+        }
+        #endregion
+    }
+}
---
+++ Ecommerce.Models/TbNumberOfPayment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Models
+{
+    public class TbNumberOfPayment
+    {
+        public TbNumberOfPayment()
+        {
+            ICollection<TbTool> _TbTool = new HashSet<TbTool>();
+        }
+        [Key]
+        public int NumberOfPaymentId { get; set; }
+
+        [Required]
+        public int ToolCurrentState { get; set; }
+
+        [Required]
+        public int NumberOfPayment { get; set; }
+
+        /*****************************/
+        public virtual ICollection<TbTool> _TbTool { get; set; }
+    }
+}
